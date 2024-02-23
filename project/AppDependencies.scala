398ab7490033977ee5201c1dc38993081a16acc9
peckover <56835853+peckover@users.noreply.github.com>
2024-02-23 20:02:28 +0000

    [DL-13103] uprade play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 private object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.12.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.2.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28" % "8.5.0",
     "uk.gov.hmrc" %% "play-allowlist-filter" % "1.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.74.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.12.0-play-28"
